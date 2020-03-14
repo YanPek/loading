69f7fba8939d41c7909a02a5594ea2c9f83d4981
Yan <54765921+YanPek@users.noreply.github.com>
2020-03-14 22:30:13 +0300

    Update loading.py

--- loading.py
+++ loading.py
@@ -2,59 +2,50 @@ from time import sleep
 
 try: from termcolor import cprint
 except ModuleNotFoundError:
-    print("You must install module 'colorama'\npip install termcolor")
+    print('You must install module "colorama"\npip install termcolor')
     exit()
 try: from colorama import Fore
 except ModuleNotFoundError:
-    print("You must install module 'colorama'\npip install colorama")
+    print('You must install module "colorama"\npip install colorama')
+    exit()
+
+def warn():
+    print(Fore.RED + "loading(int, char, 1 or 2, int or float, color(ex. 'red'))")
     exit()
 
 def loading(x, symbol, per, time, color):
     '''
-    Text colors:
-grey
-red
-green
-yellow
-blue
-magenta
-cyan
-white
-'''
-    a = x-1
-    b = 100/x
+    Text colors: grey, red, green, yellow, blue, magenta, cyan, white.
+    '''
+    a = x - 1
+    b = 100 / x
     c = 0
-    warn = Fore.RED+"loading(int, char, 1 or 2, int or float, color(ex. 'red'))"
 
     #checking
-    if type(x) != type(int()):
-        print(warn)
-        exit()
+    if type(x) is not int:
+        warn()
 
-    if type(time) not in [type(float()), type(int())]:
-        print(warn)
-        exit()
+    if type(time) not in [float, int]:
+        warn()
 
-    if len(symbol)>1:
-        print(warn)
-        exit()
+    if len(symbol) > 1:
+        warn()
     
     
     for i in range(x):
-        c = c+b
+        c = c + b
         if per == 1:
-            cprint('['+symbol*i+' '*a+']' + str(round(c))+'% ', color, end='\r')
+            cprint('[' + symbol * i + ' ' * a + ']' + str(round(c)) + '% ', color, end='\r')
         elif per == 2:
-            cprint('['+symbol*i+' '*a+']', color, end='\r')
+            cprint('[' + symbol * i + ' ' * a + ']', color, end='\r')
         else:
-            print(warn)
-            exit()
-        a-=1
+            warn()
+        a -= 1
         sleep(time)
 
-loading(50, 'о', 1, .1, 'green')
-#60 is count of characters
-#w is the display character
-#1 is when interest is shown, but 2 is notv
-#.1 is the interval per seconds
-#red is color of p-bar
+# 60  is count of characters
+# w   is the display character
+# 1   is when interest is shown, but 2 is notv
+# .1  is the interval per seconds
+# red is color of p-bar
+loading(60, 'w', 1, .1, 'red')

4433da93bf0931f73f0796dbae7f4e5e4be2981a
Yan <54765921+YanPek@users.noreply.github.com>
2020-03-15 01:09:19 +0300

    Update loading.py

--- loading.py
+++ loading.py
@@ -2,50 +2,82 @@ from time import sleep
 
 try: from termcolor import cprint
 except ModuleNotFoundError:
-    print('You must install module "colorama"\npip install termcolor')
+    print("You must install module 'termcolor'\npip install termcolor")
     exit()
 try: from colorama import Fore
 except ModuleNotFoundError:
-    print('You must install module "colorama"\npip install colorama')
+    print("You must install module 'colorama'\npip install colorama")
     exit()
-
-def warn():
-    print(Fore.RED + "loading(int, char, 1 or 2, int or float, color(ex. 'red'))")
+try: from colored import fg
+except ModuleNotFoundError:
+    print("You must install module 'colored'\npip install colored")
     exit()
 
 def loading(x, symbol, per, time, color):
     '''
-    Text colors: grey, red, green, yellow, blue, magenta, cyan, white.
-    '''
-    a = x - 1
-    b = 100 / x
+    Text colors:
+grey
+red
+green
+yellow
+blue
+magenta
+cyan
+white
+rb(rainbow)
+'''
+    a = x-1
+    b = 100/x
     c = 0
+    warn = Fore.RED+"loading(int, char, 1 or 2, int or float, color(ex. 'red'))"
 
     #checking
-    if type(x) is not int:
-        warn()
+    if type(x) != type(int()):
+        print(warn)
+        exit()
 
-    if type(time) not in [float, int]:
-        warn()
+    if type(time) not in [type(float()), type(int())]:
+        print(warn)
+        exit()
 
-    if len(symbol) > 1:
-        warn()
+    if len(symbol)>1:
+        print(warn)
+        exit()
     
-    
+    col = [45, 44, 43, 42, 41, 40, 106, 149, 142, 154, 11, 167, 202, 203, 204, 205, 206, 207, 13, 5, 127, 128, 129, 92, 93, 56, 57, 69, 39, 43, 44, 45]
+    l=1
     for i in range(x):
-        c = c + b
-        if per == 1:
-            cprint('[' + symbol * i + ' ' * a + ']' + str(round(c)) + '% ', color, end='\r')
-        elif per == 2:
-            cprint('[' + symbol * i + ' ' * a + ']', color, end='\r')
+        c = c+b
+        if per == 2:
+            if color == 'rb':
+                
+                if l == 32:l=1
+                
+                cprint(fg(col[l])+'['+symbol*i+' '*a+']', end='\r')
+                l+=1
+                
+            else:cprint('['+symbol*i+' '*a+']', color, end='\r')
+                
+        elif per == 1:
+            
+            if color == 'rb':
+                
+                if l == 32:l=1
+                
+                cprint(fg(col[l])+'['+symbol*i+' '*a+']' + str(round(c))+'% ', end='\r')
+                l+=1
+                
+            else:cprint('['+symbol*i+' '*a+']' + str(round(c))+'% ', color, end='\r')
+                
         else:
-            warn()
-        a -= 1
+            print(warn)
+            exit()
+        a-=1
         sleep(time)
 
-# 60  is count of characters
-# w   is the display character
-# 1   is when interest is shown, but 2 is notv
-# .1  is the interval per seconds
-# red is color of p-bar
-loading(60, 'w', 1, .1, 'red')
+loading(50, 'о', 2, .1, 'red')
+#60 is count of characters
+#w is the display character
+#1 is when interest is shown, but 2 is not
+#.1 is the interval per seconds
+#red is color of p-bar
